4c1d0db1859bc625f947d32b0e02db8864b0a53b
gedsHuster <ds.ge@qq.com>
2020-08-16 10:53:44 +0800

    完成代码注释添加

--- Chapter9_Template_and_Group_Data/ListNode_and_List.h
+++ Chapter9_Template_and_Group_Data/ListNode_and_List.h
@@ -132,11 +132,15 @@ void List<T>::copyNodes(ListNode<T> *p, int num) {
     }
 }
 
+/** @brief 构造函数
+ * */
 template <class T>
 List<T>::List() {
     init();
 }
 
+/** @brief 析构函数
+ * */
 template <class T>
 List<T>::~List(){
     clear();
@@ -144,46 +148,77 @@ List<T>::~List(){
     delete trailer;
 }
 
+/** @brief 复制构造函数, 复制整个链表
+ *  @param L 用于复制的链表
+ * */
 template <class T>
 List<T>::List(const List<T>& L) {
     copyNodes(L.first(), L.size_);
 }
 
+/** @brief 复制构造函数, 复制链表的给定区间
+ *  @param L 用于复制的链表
+ *  @param pos 复制区间的第一个节点在链表 L中的索引
+ *  @param num 复制节点的个数
+ * */
 template <class T>
 List<T>::List(const List<T>& L, int pos, int num) {
     copyNodes(L[pos], num);
 }
 
+/** @brief 复制构造函数, 复制链表的给定区间
+ *  @param p 复制区域的第一个节点
+ *  @param num 复制节点的个数
+ * */
 template <class T>
 List<T>::List(ListNode<T>* p, int num) {
     copyNodes(p, num);
 }
 
+/** @brief 查询链表中的节点个数
+ *  @return 链表中的节点个数
+ * */
 template <class T>
 int List<T>::size() const {
     return size_;
 }
 
+/** @brief 判断链表是否为空
+ *  @return true: 空. false: 非空.
+ * */
 template <class T>
 bool List<T>::empty() const {
     return size_ <= 0;
 }
 
+/** @brief 判断节点是否合法, 若节点不在链表区间内, 则非法
+ *  @return true: 合法. false: 非法.
+ * */
 template <class T>
 bool List<T>::valid(ListNode<T> *p) const {
     return p && (header != p) && (trailer != p);
 }
 
+/** @brief 查询链表中的首位节点
+ *  @return 链表中的首位节点指针
+ * */
 template <class T>
 ListNode<T>* List<T>::first() const{
     return header->succ;
 }
 
+/** @brief 查询链表中最后一个节点
+ *  @return 链表中最后一个节点指针
+ * */
 template <class T>
 ListNode<T>* List<T>::last() const {
     return trailer->pred;
 }
 
+/** @brief 重载下标运算符‘[]’,
+ *  @param pos 要索引的下标
+ *  @return 链表在该索引处的节点指针
+ * */
 template <class T>
 ListNode<T>* List<T>::operator[](int pos) const {
     ListNode<T> *p;
@@ -199,31 +234,54 @@ ListNode<T>* List<T>::operator[](int pos) const {
     return p;
 }
 
+/** @brief 给定一个数据元素, 构造节点, 将节点插入到链表首位
+ *  @param e 用于构造和插入节点的数据元素
+ *  @return 该插入节点的指针
+ * */
 template <class T>
 ListNode<T>* List<T>::insertElemAsFirstNode(const T &e) {
     size_++;
     return header->insertElemAsSuccNode(e);
 }
 
+/** @brief 给定一个数据元素, 构造节点, 将节点插入到链表尾部
+ *  @param e 用于构造和插入节点的数据元素
+ *  @return 该插入节点的指针
+ *  @attention 注意！ 一定是trailer前插, 不是header前插!
+ *             否则段错误, 因为header->pred无定义, 所以那段内存被保护, 无法操作
+ * */
 template <class T>
 ListNode<T>* List<T>::insertElemAsLastNode(const T &e) {
     size_++;
-    /** 注意！ 一定是trailer前插, 不是header前插! 否则段错误, 因为header->pred无定义, 故那段内存被保护 **/
     return trailer->insertElemAsPredNode(e);
 }
 
+/** @brief 在给定节点后面插入一个节点
+ *  @param p 节点
+ *  @param e 用于构造和插入节点的数据元素
+ *  @return 该插入节点的指针
+ * */
 template <class T>
 ListNode<T>* List<T>::insertElemAfterNode(ListNode<T> *p, const T &e) {
     size_++;
     return p->insertElemAsSuccNode(e);
 }
 
+/** @brief 在给定节点前面插入一个节点
+ *  @param p 节点
+ *  @param e 用于构造和插入节点的数据元素
+ *  @return 该插入节点的指针
+ * */
 template <class T>
 ListNode<T>* List<T>::insertElemBeforeNode(ListNode<T> *p, const T &e) {
     size_++;
     return p->insertElemAsPredNode(e);
 }
 
+/** @brief 从链表中删除给定节点
+ *  @param p 节点
+ *  @return p的数据元素
+ * */
 template <class T>
 T List<T>::remove(ListNode<T> *p) {
     T e = p->data;
@@ -234,6 +292,9 @@ T List<T>::remove(ListNode<T> *p) {
     return e;
 }
 
+/** @brief 遍历链表的每个节点, 并用函数指针处理节点中的数据元素
+ *  @param visit void类型的函数指针, 有一个T类型的参数
+ * */
 template <class T>
 void List<T>::traverse(void (*visit)(T &)) {
     for(ListNode<T>* p = header->succ; p != trailer; p = p->succ)
